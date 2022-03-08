0e4483c80349d8ac86ecfdddc13d6e618a165749
tessavivian <tessaschouten@valori.nl>
2022-03-08 02:49:47 +0100

    functional components

--- src/App.js
+++ src/App.js
@@ -1,73 +1,52 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import KillerList from "./components/killer-list/killer-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [kitties, setKitties] = useState([]);
+  const [filteredKitties, setFilteredKitties] = useState(kitties);
+  const [selectedKiller, setKiller] = useState({});
 
-    this.state = {
-      kitties: [],
-      searchField: "",
-      selectedKiller: {},
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { kitties: users };
-        })
-      );
-  }
+      .then((users) => setKitties(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-
-    this.setState(() => {
-      return { searchField };
+  useEffect(() => {
+    const newFilteredKitties = kitties.filter((kitty) => {
+      return kitty.username.toLocaleLowerCase().includes(searchField);
     });
+
+    setFilteredKitties(newFilteredKitties);
+  }, [kitties, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  onSelectKiller = (event) => {
-    const selectedKiller = event.target.parentElement;
-    selectedKiller.classList.add("selected-killer");
-    console.log(selectedKiller);
-
-    this.setState(() => {
-      return { selectedKiller };
-    });
+  const onSelectKiller = (event) => {
+    const selectedKillerKitty = event.target.parentElement;
+    setKiller(selectedKillerKitty);
+    selectedKillerKitty.classList.add("selected-killer");
   };
 
-  render() {
-    const { kitties, searchField } = this.state;
-    const { onSearchChange } = this;
-    const { onSelectKiller } = this;
-
-    const filteredKitties = kitties.filter((kitty) => {
-      return kitty.name.toLocaleLowerCase().includes(searchField);
-    });
-
-    return (
-      <div className="App">
-        <h1 className="app-title">Killer Kitty</h1>
-        <p className="app-intro">Pick your killer to start the game</p>
-        <SearchBox
-          className="kitties-search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="search kitty"
-        />
-        <KillerList
-          kitties={filteredKitties}
-          onSelectHandler={onSelectKiller}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1 className="app-title">Killer Kitty</h1>
+      <p className="app-intro">Pick your killer to start the game</p>
+      <SearchBox
+        className="kitties-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search kitty"
+      />
+      <KillerList kitties={filteredKitties} onSelectHandler={onSelectKiller} />
+    </div>
+  );
+};
 
 export default App;
